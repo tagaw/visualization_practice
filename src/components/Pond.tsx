d9e3418c12a1842ec70472d518f7921ecff8d3c6
Tyler Gaw <tagaw@ucsc.edu>
2026-01-29 19:33:34 -0800

    food position makes use of state

--- src/components/Pond.tsx
+++ src/components/Pond.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Tadpole from "./Tadpole";
 
 export type props = { 
@@ -11,78 +11,86 @@ type pondFlagType = {
 }
 
 export default function Pond({tadpoleCt} : props) {
+  // TODO: make tadpoles reactive to Pond state
 
-    const flagsRef = useRef<pondFlagType>({flagArr: new Array(tadpoleCt).fill(false), flagCt: tadpoleCt});
-    const targetRef = useRef<[number, number] | null>(null); 
+  // Food and its state | ref is used for direct SVG manipulation for performance 
+  const foodRef = useRef<SVGCircleElement | null>(null);
+  const [foodX, setFoodX] = useState<number>(-100);
+  const [foodY, setFoodY] = useState<number>(-100);
 
-    const canvasRef = useRef<SVGSVGElement | null>(null);
-    const foodRef = useRef<SVGCircleElement | null>(null);
+  // refs to allow tadpoles to update pond state
+  // TODO: make use of state (maybe?)
+  const flagsRef = useRef<pondFlagType>({flagArr: new Array(tadpoleCt).fill(false), flagCt: tadpoleCt});
+  const targetRef = useRef<[number, number] | null>(null); 
 
-    const foodSize = Math.sqrt(tadpoleCt) * 1.25 + 5;
-    const foodDecrement = foodSize / tadpoleCt;
+  // used to get position of click interaction
+  const canvasRef = useRef<SVGSVGElement | null>(null);
+  
 
-    const frameId = useRef<number | null>(null);
+  // Constants and ref to clean up animation frame
+  const foodSize = Math.sqrt(tadpoleCt) * 1.25 + 5;
+  const foodDecrement = foodSize / tadpoleCt;
 
-    const [state, setState] = React.useState<number>(0); 
+  const frameId = useRef<number | null>(null);
 
-    function cleanupState() {
-      targetRef.current = null;
-      foodRef.current?.setAttribute("r", "0");
-      flagsRef.current = {flagArr: new Array(tadpoleCt).fill(false), flagCt: tadpoleCt};
-      frameId.current && cancelAnimationFrame(frameId.current);
-    }
+  const [state, setState] = React.useState<number>(0); 
 
-    useEffect(() => { 
-      // when tadpole count changes, reset the pond state
+  function cleanupState() {
+    targetRef.current = null;
+    foodRef.current?.setAttribute("r", "0");
+    flagsRef.current = {flagArr: new Array(tadpoleCt).fill(false), flagCt: tadpoleCt};
+    frameId.current && cancelAnimationFrame(frameId.current);
+  }
+
+  useEffect(() => { 
+    // when tadpole count changes, reset the pond state
+    cleanupState();
+
+    return () => {
       cleanupState();
-
-      return () => {
-        cleanupState();
-      }
-    }, [tadpoleCt]);
-
-    function dropFood(event: React.MouseEvent<SVGSVGElement, MouseEvent>) {
-      if (!targetRef.current) {
-        const boundingBox = canvasRef.current?.getBoundingClientRect();
-
-        const x = event.clientX - (boundingBox?.left || 0);
-        const y = event.clientY - (boundingBox?.top || 0);
-
-        targetRef.current = [x, y];
-        
-        // Create an animation to clear the target
-        foodRef.current?.setAttribute("r", foodSize.toString());
-        foodRef.current?.setAttribute("cx", x.toString());
-        foodRef.current?.setAttribute("cy", y.toString());
-        
-
-        function foodAnimation() {
-           if (flagsRef.current.flagCt > 0) {
-              const tadpolesEaten = tadpoleCt - flagsRef.current.flagCt;
-              foodRef.current?.setAttribute("r", (foodSize - (tadpolesEaten * foodDecrement)).toString());
-              frameId.current = requestAnimationFrame(foodAnimation);
-           } else {
-              targetRef.current = null;
-              foodRef.current?.setAttribute("r", "0");
-              flagsRef.current.flagArr = flagsRef.current.flagArr.map(() => false);
-              flagsRef.current.flagCt = tadpoleCt;
-           }
-        }
-
-        frameId.current = requestAnimationFrame(foodAnimation);
-      } 
     }
-     
+  }, [tadpoleCt]);
 
-    return (
-        <>
-        <button onClick={() => setState(state+1)}>ffff</button>
-        <div className='w-250 h-150 border-4 flex-none'>
-          <svg ref={canvasRef} className='w-full h-full' onClick={dropFood}>
-            <circle ref={foodRef} r={0} fill="brown" />
-            {new Array(tadpoleCt).fill(0).map((_,index) => (<Tadpole key={index}  target={targetRef} flags={flagsRef} id={index} />))}
-          </svg>
-        </div>
-        </>
-    );
+  function dropFood(event: React.MouseEvent<SVGSVGElement, MouseEvent>) {
+    if (!targetRef.current) {
+      const boundingBox = canvasRef.current?.getBoundingClientRect();
+
+      const x = event.clientX - (boundingBox?.left || 0);
+      const y = event.clientY - (boundingBox?.top || 0);
+
+      targetRef.current = [x, y];
+      
+      // direct dom manipulation to scale down food circle
+      setFoodX(x);
+      setFoodY(y);
+
+      foodRef.current?.setAttribute("r", foodSize.toString());
+      function foodAnimation() {
+          if (flagsRef.current.flagCt > 0) {
+            const tadpolesEaten = tadpoleCt - flagsRef.current.flagCt;
+            foodRef.current?.setAttribute("r", (foodSize - (tadpolesEaten * foodDecrement)).toString());
+            frameId.current = requestAnimationFrame(foodAnimation);
+          } else {
+            targetRef.current = null;
+            foodRef.current?.setAttribute("r", "0");
+            flagsRef.current.flagArr = flagsRef.current.flagArr.map(() => false);
+            flagsRef.current.flagCt = tadpoleCt;
+          }
+      }
+      frameId.current = requestAnimationFrame(foodAnimation);
+    } 
+  }
+    
+
+  return (
+      <>
+      <button onClick={() => setState(state+1)}>ffff</button>
+      <div className='w-250 h-150 border-4 flex-none'>
+        <svg ref={canvasRef} className='w-full h-full' onClick={dropFood}>
+          <circle ref={foodRef} cx={foodX} cy={foodY} r={0} fill="brown" />
+          {new Array(tadpoleCt).fill(0).map((_,index) => (<Tadpole key={index}  target={targetRef} flags={flagsRef} id={index} />))}
+        </svg>
+      </div>
+      </>
+  );
 }
