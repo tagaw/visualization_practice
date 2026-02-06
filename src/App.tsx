e9a6db4218c3e365a003bbc803181ac0f2702f84
Tyler Gaw <tagaw@ucsc.edu>
2026-02-05 18:16:47 -0800

    displays information about available targets to be placed

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { useRef, useState, type JSX } from 'react';
+import React, { useRef, useState } from 'react';
 import Pond from './components/Pond';
 
-export type props = {
-  pond: JSX.Element;
-}
-
-
-
 function App() {
-  const [numPoints, setNumPoints] = useState<number>(10);
+  const [numPoints, setNumPoints] = useState<number>(1);
   const numPointsRef = useRef(10);
+
+  const targetCtRef = useRef<HTMLSpanElement>(null);
+
 
   function onInput(e: React.ChangeEvent<HTMLInputElement>) {
     const val = Number(e.target.value);
@@ -34,9 +31,9 @@ function App() {
       <div className='w-full h-screen flex flex-wrap justify-center place-items-center border-4 '>
         <div className='w-250 text-center flex justify-center gap-4 mb-4'> 
           <p>Current points: {numPoints} </p>
-          <p>Current food: {}</p>
+          <p>Current food: <span ref={targetCtRef}>{100}</span></p>
         </div>
-        <Pond tadpoleCt={numPoints} />
+        <Pond tadpoleCt={numPoints} foodCt={100} counter={targetCtRef as React.RefObject<HTMLSpanElement>} />
         <div className='w-full flex justify-center gap-4'>
           <button className='border-2 rounded-md px-2' onClick={handleNewCount}>Update Tadpole Count</button>
           <span>
